7264545e323871866ad320c2fd4fea76871b3ab7
Zero <zero.sp12@gmail.com>
2023-01-09 17:00:50 -0300

    mejorado click en la tarjeta del producto para q no se vaya sin querer al view

--- src/components/TarjetaProductos.vue
+++ src/components/TarjetaProductos.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="col-md-4 py-2 mb-3">
-    <router-link :to="`/producto/${producto.id}`">
-    <div class="card h-100 text-center user-select-none">
-      <img :src="producto.image" class="card-img-top" />
-      <div class="card-body">
-        <h6 class="card-title fw-bold">{{ producto.nombre }}</h6>
+      <div class="card h-100 text-center user-select-none" @click="verProducto(producto.id)">
+        <img :src="producto.image" class="card-img-top" />
+        <div class="card-body">
+          <h6 class="card-title fw-bold">{{ producto.nombre }}</h6>
+        </div>
+        <div class="card-footer d-grid gap-2">
+          <button class="btn fw-bold btn-sm btn-warning py-2" :disabled="chequearBoton"
+            @click.prevent="addProductToCart($event)">
+            <i class="fas fa-shopping-cart mr-2 btn-block"></i> $ {{ producto.precio }}
+          </button>
+        </div>
       </div>
-      <div class="card-footer d-grid gap-2">
-        <button
-          class="btn fw-bold btn-sm btn-warning py-2"
-          :disabled="chequearBoton"
-          @click.prevent="addProductToCart($event)"
-        >
-          <i class="fas fa-shopping-cart mr-2 btn-block"></i> $ {{ producto.precio }}
-        </button>
-      </div>
-    </div>
-      </router-link>
   </div>
 </template>
 <script>
@@ -39,24 +34,30 @@ export default {
   methods: {
     ...mapMutations('carrito', ['insertarProducto']),
 
+    verProducto(index) {
+
+      if (this.botonActivo == false) {
+        this.$router.push(`/producto/${index}`)
+      }
+    },
     addProductToCart(event) {
-        this.botonActivo = true;
+      this.botonActivo = true;
 
-        var target = event.currentTarget;
-        target.innerHTML = '<i class="fas fa-check mr-2"></i> <b>Agregado!</b>';
+      var target = event.currentTarget;
+      target.innerHTML = '<i class="fas fa-check mr-2"></i> <b>Agregado!</b>';
 
-        this.insertarProducto({
-          id_producto: Number(this.producto.id),
-          precio: this.producto.precio,
-          cantidad: 1,
-          usuario: Number(localStorage.clientID),
-        });
+      this.insertarProducto({
+        id_producto: Number(this.producto.id),
+        precio: this.producto.precio,
+        cantidad: 1,
+        usuario: Number(localStorage.clientID),
+      });
 
-        setTimeout(() => {
-          target.innerHTML =
-            '<i class="fas fa-shopping-cart mr-2"></i> Agregar al Carrito';
-          this.botonActivo = false;
-        }, 100);
+      setTimeout(() => {
+        target.innerHTML =
+          '<i class="fas fa-shopping-cart mr-2"></i> Agregar al Carrito';
+        this.botonActivo = false;
+      }, 800);
     },
   },
 };
@@ -70,17 +71,21 @@ export default {
   width: 95%;
   margin: 1% !important;
 }
+
 a {
   text-decoration: none;
   color: #333;
 }
+
 .row>* {
   padding: 4px !important;
 }
+
 .card:hover {
   cursor: pointer;
   box-shadow: 0px 0px 6px 2px #bbb;
 }
+
 .card-footer {
   background-color: white !important;
   border-top: 0px;
